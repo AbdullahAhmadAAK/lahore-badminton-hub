63d77057f531c499dcadf707f72e33aa975f24b7
Abdullah Ahmad <abdullahaak06@gmail.com>
2024-06-18 00:33:08 +0500

    add some todos and fix issue in users#index

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,14 @@
+class AuthController < ApplicationController
+    # TODO: fill these up too
+    # TODO: what's the need for handling here btw?
+    def register
+    end
+
+    def login
+    end
+
+    def logout
+    end
+
+    # TODO: params private method
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,23 @@
 class UsersController < ApplicationController
-    
+    def index
+        # TODO: shift render_success_response and render_error_response to methods in application_controller
+        @users = User.all
+        render json: { success: true, data: @users, errors: [] }
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    # TODO: will signup login be here? why or why not?
+
+    # TODO: add params method here
 end
